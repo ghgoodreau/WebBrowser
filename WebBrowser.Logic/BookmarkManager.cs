f828c86df7e3e074330f9754337c6a0b9c47ce21
Gregory Goodreau <goodreauh@gmail.com>
2020-02-16 22:32:51 -0600

    added additem bookmark method

--- WebBrowser.Logic/BookmarkManager.cs
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBrowser.Data.BookmarksDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
     class BookmarkManager
     {
+        public static void AddItem(BookmarkItem item)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(item.Id, item.Title, item.URL);
+        }
     }
 }
