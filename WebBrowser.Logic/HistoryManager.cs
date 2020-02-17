f675b1f5e664bc6b642a329d5995b769eacd8899
Gregory Goodreau <goodreauh@gmail.com>
2020-02-16 22:54:14 -0600

    fixed an insane amount of errors.

--- WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser.Logic/HistoryManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WebBrowser.Data.HistoryDataSetTableAdapters;
 namespace WebBrowser.Logic
 {
     class HistoryManager
     {
+        public static void AddItem(HistoryItem item)
+        {
+            var adapter = new HistoryTableAdapter();
+            adapter.Insert(item.URL, item.Title, item.Date);
+        }
     }
 }
