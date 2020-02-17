414eeae5574a2a499bd4fd952f06680bdd0d54a1
Gregory Goodreau <goodreauh@gmail.com>
2020-02-16 22:57:26 -0600

    added history manager methods.

--- WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser.Logic/HistoryManager.cs
@@ -13,5 +13,24 @@ namespace WebBrowser.Logic
             var adapter = new HistoryTableAdapter();
             adapter.Insert(item.URL, item.Title, item.Date);
         }
+
+        public static List<HistoryItem> GetItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var results = new List<HistoryItem>();
+            var rows = adapter.GetData();
+
+            foreach (var row in rows)
+            {
+                var item = new HistoryItem();
+                item.URL = row.URL;
+                item.Title = row.Title;
+                item.Date = row.Date;
+                item.Id = row.Id;
+
+                results.Add(item);
+            }
+            return results;
+        }
     }
 }
