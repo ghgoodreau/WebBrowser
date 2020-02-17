197c87e72e079fe52a85149720cce9bb66ebee8c
Gregory Goodreau <goodreauh@gmail.com>
2020-02-16 23:25:33 -0600

    renamed things

--- WebBrowser.UI/Bookmark Manager Form.Designer.cs
+++ WebBrowser.UI/Bookmark Manager Form.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Bookmark_Manager_Item
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Bookmark_Manager_Item";
+            this.Name = "Bookmark_Manager_Item";
+            this.Text = "Bookmark_Manager_Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
