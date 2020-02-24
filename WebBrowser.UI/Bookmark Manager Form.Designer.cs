87039b79c53cded55afa2666fdf20dfceedc8fdc
Gregory Goodreau <goodreauh@gmail.com>
2020-02-23 19:24:09 -0600

    added bookmark search bar and button and delete button

--- WebBrowser.UI/Bookmark Manager Form.Designer.cs
+++ WebBrowser.UI/Bookmark Manager Form.Designer.cs
@@ -29,31 +29,68 @@
         private void InitializeComponent()
         {
             this.bookmarkListBox = new System.Windows.Forms.ListBox();
+            this.bookSearchTextBox = new System.Windows.Forms.TextBox();
+            this.bookSearchButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bookmarkListBox
             // 
+            this.bookmarkListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.bookmarkListBox.FormattingEnabled = true;
-            this.bookmarkListBox.Location = new System.Drawing.Point(69, 25);
+            this.bookmarkListBox.Location = new System.Drawing.Point(67, 67);
             this.bookmarkListBox.Name = "bookmarkListBox";
             this.bookmarkListBox.Size = new System.Drawing.Size(653, 381);
             this.bookmarkListBox.TabIndex = 0;
             // 
-            // Bookmark_Manager_Item
+            // bookSearchTextBox
+            // 
+            this.bookSearchTextBox.Location = new System.Drawing.Point(67, 41);
+            this.bookSearchTextBox.Name = "bookSearchTextBox";
+            this.bookSearchTextBox.Size = new System.Drawing.Size(440, 20);
+            this.bookSearchTextBox.TabIndex = 1;
+            // 
+            // bookSearchButton
+            // 
+            this.bookSearchButton.Location = new System.Drawing.Point(545, 39);
+            this.bookSearchButton.Name = "bookSearchButton";
+            this.bookSearchButton.Size = new System.Drawing.Size(75, 23);
+            this.bookSearchButton.TabIndex = 2;
+            this.bookSearchButton.Text = "search";
+            this.bookSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Bookmark_Manager_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bookSearchButton);
+            this.Controls.Add(this.bookSearchTextBox);
             this.Controls.Add(this.bookmarkListBox);
-            this.Name = "Bookmark_Manager_Item";
+            this.Name = "Bookmark_Manager_Form";
             this.Text = "Bookmark Manager";
             this.Load += new System.EventHandler(this.Bookmark_Manager_Item_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox bookmarkListBox;
+        private System.Windows.Forms.TextBox bookSearchTextBox;
+        private System.Windows.Forms.Button bookSearchButton;
+        private System.Windows.Forms.Button button1;
     }
 }
